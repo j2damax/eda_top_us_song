ccad9bcdd75c368f53ca77cd40b3243a06d79462
Jayampathy Balasuriya <jam@vfullstack.com>
2025-03-18 19:02:39 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Exploratory Data Analysis on Top US Songs from 1950 to 2020 ")
+    print("Exploratory Data Analysis on Top US Movies from 1950 to 2020")
 
 if __name__ == "__main__":
     main()
